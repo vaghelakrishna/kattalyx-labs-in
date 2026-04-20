062c47619124f052c024d09f07db45837d01bb17
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-04-20 10:48:00 +0530

    fix

---
+++ frontend/src/tabler-icons-react.d.ts
@@ -0,0 +1,29 @@
+declare module '@tabler/icons-react' {
+  import { FC, SVGProps } from 'react';
+
+  export const IconCirclePlusFilled: FC<SVGProps<SVGSVGElement>>;
+  export const IconMail: FC<SVGProps<SVGSVGElement>>;
+  export const Icon: FC<SVGProps<SVGSVGElement>>;
+  export const IconChevronDown: FC<SVGProps<SVGSVGElement>>;
+  export const IconChevronLeft: FC<SVGProps<SVGSVGElement>>;
+  export const IconChevronRight: FC<SVGProps<SVGSVGElement>>;
+  export const IconChevronsLeft: FC<SVGProps<SVGSVGElement>>;
+  export const IconChevronsRight: FC<SVGProps<SVGSVGElement>>;
+  export const IconCircleCheckFilled: FC<SVGProps<SVGSVGElement>>;
+  export const IconDotsVertical: FC<SVGProps<SVGSVGElement>>;
+  export const IconGripVertical: FC<SVGProps<SVGSVGElement>>;
+  export const IconLayoutColumns: FC<SVGProps<SVGSVGElement>>;
+  export const IconLoader: FC<SVGProps<SVGSVGElement>>;
+  export const IconPlus: FC<SVGProps<SVGSVGElement>>;
+  export const IconTrendingUp: FC<SVGProps<SVGSVGElement>>;
+  export const IconTrendingDown: FC<SVGProps<SVGSVGElement>>;
+  export const IconCreditCard: FC<SVGProps<SVGSVGElement>>;
+  export const IconLogout: FC<SVGProps<SVGSVGElement>>;
+  export const IconNotification: FC<SVGProps<SVGSVGElement>>;
+  export const IconUserCircle: FC<SVGProps<SVGSVGElement>>;
+  export const IconDots: FC<SVGProps<SVGSVGElement>>;
+  export const IconFolder: FC<SVGProps<SVGSVGElement>>;
+  export const IconShare3: FC<SVGProps<SVGSVGElement>>;
+  export const IconTrash: FC<SVGProps<SVGSVGElement>>;
+  export type Icon = FC<SVGProps<SVGSVGElement>>;
+}
