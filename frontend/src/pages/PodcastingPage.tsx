681aec5cb6f465ddbde752cf0f9254a66a76248d
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-04-20 10:51:46 +0530

    fix

--- frontend/src/pages/PodcastingPage.tsx
+++ frontend/src/pages/PodcastingPage.tsx
@@ -1,24 +1,28 @@
 import Navbar from '@/components/Navbar'
 import ServicesStack from '@/sections/brandingSection/Stack'
 import MediaCarousel from '@/sections/podcastSection/Hero'
-import PodcastHero from '@/sections/podcastSection/HeroSection'
-import MusicPortfolio from '@/sections/podcastSection/MusicPortfolio'
-import RecentPodcasts from '@/sections/podcastSection/RecentPodcasts'
+// import PodcastHero from '@/sections/podcastSection/HeroSection'
+// import MusicPortfolio from '@/sections/podcastSection/MusicPortfolio'
+// import RecentPodcasts from '@/sections/podcastSection/RecentPodcasts'
 import VideoFeed from '@/sections/podcastSection/RecentPodcasts'
 import PremiumPortfolio from '@/sections/podcastSection/Services'
-import Services from '@/sections/podcastSection/Services'
+// import Services from '@/sections/podcastSection/Services'
 import VideoListFeed from '@/sections/podcastSection/VideoListFeed'
-import React from 'react'
+import MatchedPodcastFeed from '@/sections/uniqueDesign/design'
+import HorizontalVideoFeed from '@/sections/uniqueDesign/hori'
+
 
 export default function PodcastingPage() {
   return (
     <><Navbar />
-<MediaCarousel/>
+      <MediaCarousel />
       <PremiumPortfolio />
       {/* <MusicPortfolio /> */}
-      <ServicesStack/>
+      <ServicesStack />
+      <HorizontalVideoFeed/>
       <VideoFeed />
 <VideoListFeed/>
+      <MatchedPodcastFeed/>
     </>
     
   )
